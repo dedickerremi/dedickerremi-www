ff08b029159a2c890f4838b7ee2c8f9a2ff14763
Dedicker Remi <dedickerremi@gmail.com>
2024-03-04 15:23:32 +0100

    first draft

---
+++ app/components/Footer.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+
+export const Footer = () => {
+  return (
+    <footer className="">
+      <div className="container max-w-screen-xl mx-auto px-4">
+        <div className="text-center">
+          <div className="flex items-center justify-center space-x-8">
+            <a
+              href="https://www.linkedin.com/in/remi-dedicker"
+              className="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500"
+            >
+              <Image height={16} width={16} src="/linkedin.svg" alt="linked" />
+            </a>
+
+            <a
+              href="https://github.com/dedickerremi"
+              className="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500"
+            >
+              <Image height={16} width={16} src="/github.svg" alt="github" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
