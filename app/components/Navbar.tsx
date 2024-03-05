2380a9f9f0e4615724bd26fa0265250e489c74f3
Dedicker Remi <dedickerremi@gmail.com>
2024-03-05 14:58:55 +0100

    replace cv lonk

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,12 +6,13 @@ type NavbarProps = {}
 export const Navbar = ({}: NavbarProps) => {
   return (
     <nav className="flex items-center justify-between p-8">
-      {/* <Image width={260} height={40} src="/navbar-logo.jpg" alt="Logo" /> */}
       <span className="px-7 py-3 md:px-9 md:py-4 font-medium md:font-semibold text-gray-700 text-md rounded-md ">
         {"</> Remi Dedicker"}
       </span>
       <Link
-        href={"/Remi_Dedicker.pdf"}
+        href={
+          "https://drive.google.com/file/d/1bpOQwnMChBare2D3-9TooP5rTWelWgZa/view?usp=sharing"
+        }
         target="_blank"
         rel="noopener noreferrer"
         className="px-7 py-3 md:px-9 md:py-4 bg-white font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-700 hover:text-white transition ease-linear duration-500"
