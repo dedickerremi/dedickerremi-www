9838f2ebbf499a46eaa513d949de74d4f4a5cdb9
Dedicker Remi <dedickerremi@gmail.com>
2024-04-11 19:06:40 +0200

    update logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = ({}: NavbarProps) => {
   return (
     <nav className="flex items-center justify-between p-8">
       <span className="px-7 py-3 md:px-9 md:py-4 font-medium md:font-semibold text-gray-700 text-md rounded-md ">
-        {"</> Remi Dedicker"}
+        {"< /> Remi Dedicker"}
       </span>
       <Link
         href={
@@ -15,7 +15,7 @@ export const Navbar = ({}: NavbarProps) => {
         }
         target="_blank"
         rel="noopener noreferrer"
-        className="px-7 py-3 md:px-9 md:py-4 bg-white font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-700 hover:text-white transition ease-linear duration-500"
+        className=" hidden md:block px-7 py-3 md:px-9 md:py-4 bg-white font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-700 hover:text-white"
       >
         View my CV
       </Link>
