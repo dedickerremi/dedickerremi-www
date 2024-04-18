454430c01c522f13dd39d316274171c1d02f598e
Dedicker Remi <7903181+dedickerremi@users.noreply.github.com>
2024-04-18 11:08:22 +0200

    fix error build

--- app/components/hooks/use-outside-click.ts
+++ app/components/hooks/use-outside-click.ts
@@ -1,7 +1,7 @@
 import React from "react"
 
 export const useOutsideClick = (callback: any) => {
-  const ref = React.useRef(null)
+  const ref = React.useRef<HTMLElement>(null)
 
   React.useEffect(() => {
     const handleClick = (event: any) => {
