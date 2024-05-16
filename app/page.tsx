c3fceabc6de9199f2572602af3c48e9890e55692
Dedicker Remi <7903181+dedickerremi@users.noreply.github.com>
2024-05-16 09:37:10 +0200

    unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image"
 import { FloatButtonMenu } from "./components/ui/FloatButton"
-import { useCallback, useState } from "react"
 
 export default function Home() {
   return (
